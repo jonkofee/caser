e960065a8de5a731769234b72d8c8f2fffa7b7e3
jonkofee <jonkofee@icloud.com>
2021-07-21 04:10:58 +0300

    🐛context

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package caser
+
+import (
+	"strings"
+	"unicode"
+)
+
+func CamelToSnake(str string) string  {
+	var (
+		words []string
+		temp string
+		prevChar rune
+	)
+
+	for index, char := range str {
+		if index == 0 {
+			char = unicode.ToLower(char)
+		}
+
+		if unicode.IsUpper(char) ||
+			(unicode.IsNumber(char) && prevChar != 0 && !unicode.IsNumber(prevChar)) ||
+			(unicode.IsLetter(char) && prevChar != 0 && unicode.IsNumber(prevChar)) {
+			words = append(words, temp)
+
+			temp = ""
+		}
+
+		temp += string(unicode.ToLower(char))
+
+		prevChar = char
+	}
+
+	words = append(words, temp)
+
+	return strings.Join(words, "_")
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,59 @@
 package caser
+
+import "testing"
+
+func TestUpperCamelToSnake(t *testing.T) {
+	result := CamelToSnake("UpdatedAt")
+	if result != "updated_at" {
+		t.Error("Incorrect UpperCamelCase to snake_case, got ", result)
+	}
+}
+
+func TestLowerCamelToSnake(t *testing.T) {
+	result := CamelToSnake("updatedAt")
+	if CamelToSnake("updatedAt") != "updated_at" {
+		t.Error("Incorrect lowerCamelCase to snake_case, got ", result)
+	}
+}
+
+func TestUpperCamelToSnakeWithNumber(t *testing.T) {
+	result := CamelToSnake("UpdatedAt420")
+	if CamelToSnake("UpdatedAt420") != "updated_at_420" {
+		t.Error("Incorrect UpperCamelCase to snake_case with number in end, got ", result)
+	}
+}
+
+func TestLowerCamelToSnakeWithNumber(t *testing.T) {
+	result := CamelToSnake("updatedAt420")
+	if CamelToSnake("updatedAt420") != "updated_at_420" {
+		t.Error("Incorrect lowerCamelCase to snake_case with number in end, got ", result)
+	}
+}
+
+func TestUpperCamelToSnakeStartWithNumber(t *testing.T) {
+	result := CamelToSnake("15UpdatedAt420")
+	if CamelToSnake("15UpdatedAt420") != "15_updated_at_420" {
+		t.Error("Incorrect UpperCamelCase to snake_case with number in begin, got ", result)
+	}
+}
+
+func TestLowerCamelToSnakeStartWithNumber(t *testing.T) {
+	result := CamelToSnake("15updatedAt420")
+	if CamelToSnake("15updatedAt420") != "15_updated_at_420" {
+		t.Error("Incorrect lowerCamelCase to snake_case with number in begin, got ", result)
+	}
+}
+
+func TestUpperCamelToSnakeWithNumberInMiddle(t *testing.T) {
+	result := CamelToSnake("15UpdatedAt420")
+	if CamelToSnake("15UpdatedAt420") != "15_updated_at_420" {
+		t.Error("Incorrect UpperCamelCase to snake_case with number in middle, got ", result)
+	}
+}
+
+func TestLowerCamelToSnakeWithNumberInMiddle(t *testing.T) {
+	result := CamelToSnake("15updated8At420")
+	if CamelToSnake("15updated8At420") != "15_updated_8_at_420" {
+		t.Error("Incorrect lowerCamelCase to snake_case with number in middle, got ", result)
+	}
+}
